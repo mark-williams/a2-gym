b8d7363dd3000d0776461335640c80d34c292ce1
mark-williams <markwill@hotmail.co.uk>
2016-08-15 17:39:30 +0100

    Added sone (very) rudimentary styling using Material Design

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './layout/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
-    directives: [DashboardComponent]
+    directives: [HeaderComponent, DashboardComponent]
 })
 export class AppComponent {
 }
---
+++ app/layout/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'gym-header',
+    templateUrl: 'app/layout/header.component.html'
+})
+export class HeaderComponent {
+}
