2b56eaa44e1a1732af3f80003e91f440084f7648
mark-williams <markwill@hotmail.co.uk>
2016-08-19 12:28:16 +0100

    Added an interface to type the session data

--- app/shared/instructor.model.ts
+++ app/shared/instructor.model.ts
@@ -1,4 +1,12 @@
+interface ISession {
+    time: string,
+    client: string
+}
+
 export class Instructor {
-    constructor(public id: number, public name: string, public sessionCost: number, public sessions: any[]) {
+    constructor(public id: number, public name: string, public sessionCost: number, public sessions: ISession[]) {
     }
 }
+
+
+
